b0b8d9c4b42506420ffa0e5ff42c6359caea712f
Ostap Mykhaylyak <me@ostap.dev>
2025-08-05 12:32:45 +0200

    Update Routes.php

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -1,13 +1,3 @@
 <?php
 
-namespace Ostap\Gate\Config;
-
-use CodeIgniter\Router\RouteCollection;
-
-class Routes
-{
-    public static function routes(RouteCollection $routes)
-    {
-        $routes->get('login', 'Ostap\Gate\Controllers\Auth::login');
-    }
-}
+$routes->get('login', 'Ostap\Gate\Controllers\Auth::login');
